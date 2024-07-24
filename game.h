019575c3db05a74127b480ad04e0311811cac807
김예은/연구원/리빙솔루션제어QE팀(yeeun99.kim@lge.com) <yeeun99.kim@lge.com>
2024-07-24 10:59:34 +0900

    edit plane arr

--- game.h
+++ game.h
@@ -7,14 +7,17 @@
 #define PLANE_W 			28
 #define PLANE_H 			24
 #define PLANE_SIZE			208
+#define PLANE_COLOR			0x55D
 
 #define MISSILE_W			8
 #define MISSILE_H			8
 #define MISSILE_SIZE		20
+#define MISSILE_COLOR		0xFBE4
 
 #define BULLET_W			8
 #define BULLET_H			8
 #define BULLET_SIZE	    	20
+#define BULLET_COLOR        0xFFFF
 
 #define DIGIT_WIDTH         10
 #define DIGIT_HEIGHT        14
